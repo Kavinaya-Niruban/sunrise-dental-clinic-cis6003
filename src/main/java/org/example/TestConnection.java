34d4018b28268a0513d716c0ecdf4f98a88855b3
Kavinaya-Niruban <kavinaya2006@gmail.com>
2026-09-03 14:11:11 +0530

    Add MySQL database connection

---
+++ src/main/java/org/example/TestConnection.java
@@ -0,0 +1,25 @@
+
+
+package org.example;
+
+import org.example.util.DBConnection;
+
+import java.sql.Connection;
+
+public class TestConnection {
+
+    public static void main(String[] args) {
+
+        try {
+            Connection connection = DBConnection.getConnection();
+
+            System.out.println("Database connected successfully!");
+
+            connection.close();
+
+        } catch (Exception e) {
+            System.out.println("Database connection failed!");
+            e.printStackTrace();
+        }
+    }
+}
