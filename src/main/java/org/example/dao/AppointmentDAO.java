5973533f19be01d282d95400372edfbf7a1fa8b4
Kavinaya-Niruban <kavinaya2006@gmail.com>
2026-09-04 15:05:32 +0530

    Help and Logout functions added

--- src/main/java/org/example/dao/AppointmentDAO.java
+++ src/main/java/org/example/dao/AppointmentDAO.java
@@ -25,8 +25,14 @@ public class AppointmentDAO {
             statement.setInt(2, appointment.getPatientId());
             statement.setInt(3, appointment.getDentistId());
             statement.setInt(4, appointment.getTreatmentId());
-            statement.setDate(5, java.sql.Date.valueOf(appointment.getAppointmentDate()));
-            statement.setTime(6, java.sql.Time.valueOf(appointment.getAppointmentTime()));
+            statement.setDate(
+                    5,
+                    java.sql.Date.valueOf(appointment.getAppointmentDate())
+            );
+            statement.setTime(
+                    6,
+                    java.sql.Time.valueOf(appointment.getAppointmentTime())
+            );
 
             statement.executeUpdate();
 
@@ -53,19 +59,20 @@ public class AppointmentDAO {
 
             statement.setString(1, appointmentNumber);
 
-            ResultSet resultSet = statement.executeQuery();
+            try (ResultSet resultSet = statement.executeQuery()) {
 
-            if (resultSet.next()) {
+                if (resultSet.next()) {
 
-                return new Appointment(
-                        resultSet.getInt("appointment_id"),
-                        resultSet.getString("appointment_number"),
-                        resultSet.getInt("patient_id"),
-                        resultSet.getInt("dentist_id"),
-                        resultSet.getInt("treatment_id"),
-                        resultSet.getDate("appointment_date").toLocalDate(),
-                        resultSet.getTime("appointment_time").toLocalTime()
-                );
+                    return new Appointment(
+                            resultSet.getInt("appointment_id"),
+                            resultSet.getString("appointment_number"),
+                            resultSet.getInt("patient_id"),
+                            resultSet.getInt("dentist_id"),
+                            resultSet.getInt("treatment_id"),
+                            resultSet.getDate("appointment_date").toLocalDate(),
+                            resultSet.getTime("appointment_time").toLocalTime()
+                    );
+                }
             }
 
         } catch (Exception e) {
